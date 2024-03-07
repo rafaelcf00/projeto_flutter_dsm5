d9192b4f1df731413cf44dcdc919bb3915df7000
XandyGomes <x3.computadores@hotmail.com>
2024-03-07 14:42:42 -0300

    Aula.02.03.2024

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,45 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          body: const Column(
+            children: [
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on, color: Colors.green),
+                  title: Text('7904-3'),
+                  subtitle: Text('R\$ 1000.00'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on, color: Colors.green),
+                  title: Text('1234-5'),
+                  subtitle: Text('R\$ 500.00'),
+                ),
+              ),
+                            Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on, color: Colors.green),
+                  title: Text('5478-2'),
+                  subtitle:Text('R\$ 300.00'),
+                ),
+              ),
+            ],
+          ),
           appBar: AppBar(
-            title: Text('Transferência'),
+            backgroundColor: Colors.blue,
+            title: const Text(
+              'Transferência',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
           floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.blue,
             onPressed: () {},
-            child: Icon(Icons.add),
+            child: const Icon(Icons.add, color: Colors.white),
           ),
         ),
       ),
